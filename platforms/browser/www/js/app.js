61c3f53391dc2c3bbe0e626dc9b960e47988ffba
milosmladenovic5 <milosml@outlook.com>
2017-04-24 01:48:26 +0200

    k

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,7 +1,15 @@
-$('#select').click(function(){
-  $.post(" 192.168.0.106:8000/getCityByName", {}, function(data){
-         $.each(data, function(index, value){
-            alert(data.Name);
-         });
-    });
-})
+document.addEventListener("deviceready", init, false);
+
+function init() {
+//listen for button clicks
+        document.querySelector("#firstBtn").addEventListener("touchend", function() {
+            // navigator.notification.alert(
+            //   "This is a test...", null,"Alert Test", "OK!");
+            $.post('http://192.168.0.17:8000/api/getCityByName',{}, function(data){
+                navigator.notification.alert(data.Name, null, "Test", "OK");
+            });
+
+          }, false);
+
+
+}
