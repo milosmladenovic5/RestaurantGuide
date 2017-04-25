516f4e36f9cd1bdd70083e181914bbc7d9b8191e
milosmladenovic5 <milosml@outlook.com>
2017-04-25 12:02:52 +0200

    big changes

--- typings/cordova-typings.d.ts
+++
@@ -1,5 +0,0 @@
-
-/// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
-/// <reference path="..\.vscode\typings\cordova\plugins\Contacts.d.ts"/>
-/// <reference path="..\.vscode\typings\cordova\plugins\Camera.d.ts"/>
-/// <reference path="..\.vscode\typings\cordova\plugins\FileSystem.d.ts"/>
