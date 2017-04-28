3d6b04aebdcf07082243cb4f6c29f09e7fda2d4e
0508994 <m.n.milan@hotmail.com>
2017-04-28 12:53:23 +0200

    tab rename

--- RestaurantGuide_v5/src/app/app.module.ts
+++ RestaurantGuide_v5/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { FavoritesPage } from '../pages/favorites/favorites';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { PlacesListPage } from '../pages/placesList/placesList';
@@ -22,7 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    FavoritesPage,
     HomePage,
     TabsPage,
     PlacesListPage,
@@ -39,7 +39,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    FavoritesPage,
     HomePage,
     TabsPage,
     PlacesListPage,
--- RestaurantGuide_v5/src/pages/favorites/favorites.ts
+++ RestaurantGuide_v5/src/pages/favorites/favorites.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-favorites',
+  templateUrl: 'favorites.html'
 })
-export class ContactPage {
+export class FavoritesPage {
 
   constructor(public navCtrl: NavController) {
 
